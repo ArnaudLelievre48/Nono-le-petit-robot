3113502cf297808cf1e98027f5f6e8db504ee232
ArnaudLelievre48 <lolmyizi@gmail.com>
2026-04-14 00:09:50 +0200

    little update

--- Nono.py
+++ Nono.py
@@ -18,8 +18,11 @@ from PIL import Image
 
 # ================= CONFIG =================
 BOT_TOKEN = ""
-with open("bot-token.txt","r") as bot_token:
-    BOT_TOKEN = bot_token.read()
+try:
+    with open("bot-token.txt","r") as bot_token:
+        BOT_TOKEN = bot_token.read()*
+except:
+    pass
 
 DISCORD_TOKEN = "YOUR_TOKEN"
 
